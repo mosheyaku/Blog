f84216b9f8b38053ce95af03c05198ba1983101b
Moshe Yaku <mosheyaku@gmail.com>
2023-10-23 21:42:12 +0300

    Creating initial views

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.start_page, name="start_page"),
+    path("posts", views.posts, name="posts_page"),
+    path("posts/<slug:slug>", views.post_details, name="post_details-page"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def start_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_details(request):
+    pass
