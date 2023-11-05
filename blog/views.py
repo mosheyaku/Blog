20e0d65a0783b6e8e0e6ac3dbffe771020f36e66
Moshe Yaku <mosheyaku@gmail.com>
2023-11-05 20:17:04 +0200

    Loading data to posts page from the database we've created.

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,7 @@ def start_page(request):
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
